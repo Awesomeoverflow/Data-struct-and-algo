08c1e9f111fa8c16b75ba64c74dc7c4fbe011574
Harshit Patel <127538212+Awesomeoverflow@users.noreply.github.com>
2025-02-03 10:34:06 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 75.64%] - LeetPush

---
+++ 74-Search-a-2D-Matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(0,len(matrix)):
+            if target>=matrix[i][0] and target<=matrix[i][-1]:
+                for j in range(0,len(matrix[i])):
+                    if matrix[i][j]==target:
+                        return True
+        return False 
+
+            
